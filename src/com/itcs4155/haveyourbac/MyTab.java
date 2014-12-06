289069038c627a81e534804b1534855b48f5c5f9
Tim Johnson <tjohn92@gmail.com>
2014-12-06 15:58:24 -0500

    Made the myTab page

---
+++ src/com/itcs4155/haveyourbac/MyTab.java
@@ -0,0 +1,15 @@
+package com.itcs4155.haveyourbac;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MyTab extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_my_tab);
+	}
+}
