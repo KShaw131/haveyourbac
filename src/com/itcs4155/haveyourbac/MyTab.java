3c2f345af2ee16716d6bef967f9cc5e57a0e76cb
Tim Johnson <tjohn92@gmail.com>
2014-12-06 21:24:53 -0500

    Hoping the uber button works

--- src/com/itcs4155/haveyourbac/MyTab.java
+++ src/com/itcs4155/haveyourbac/MyTab.java
@@ -9,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MyTab extends Activity {
 
@@ -17,15 +18,13 @@ public class MyTab extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_my_tab);
 		
-		final Button uber = (Button)findViewById(R.id.uberButton);
+		final ImageButton uber = (ImageButton)findViewById(R.id.uberButton);
 		
 			uber.setOnClickListener(new View.OnClickListener(){
 				
         	public void onClick(View view){
         		//Used to open the uber app
         		Intent i;
-        		String url = "http://www.example.com";
-        		
         		PackageManager manager = getPackageManager();
         		try {
         		   i = manager.getLaunchIntentForPackage("com.ubercab");
@@ -34,7 +33,9 @@ public class MyTab extends Activity {
         		i.addCategory(Intent.CATEGORY_LAUNCHER);
         		startActivity(i);
         		} catch (PackageManager.NameNotFoundException e) {
-        				
+        			
+        			startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://search?q=pub:com.ubercab") ) );
+        		    
         		}     		
         	}
         
