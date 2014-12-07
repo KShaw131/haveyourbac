8c57dc7bb41c71b09e2c68addc37f47f8069d841
Tim Johnson <tjohn92@gmail.com>
2014-12-06 20:05:13 -0500

    Lets the Uber button do things

--- src/com/itcs4155/haveyourbac/MyTab.java
+++ src/com/itcs4155/haveyourbac/MyTab.java
@@ -1,9 +1,14 @@
 package com.itcs4155.haveyourbac;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MyTab extends Activity {
 
@@ -11,5 +16,29 @@ public class MyTab extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_my_tab);
+		
+		final Button uber = (Button)findViewById(R.id.uberButton);
+		
+			uber.setOnClickListener(new View.OnClickListener(){
+				
+        	public void onClick(View view){
+        		//Used to open the uber app
+        		Intent i;
+        		String url = "http://www.example.com";
+        		
+        		PackageManager manager = getPackageManager();
+        		try {
+        		   i = manager.getLaunchIntentForPackage("com.ubercab");
+        		if (i == null)
+        		    throw new PackageManager.NameNotFoundException();
+        		i.addCategory(Intent.CATEGORY_LAUNCHER);
+        		startActivity(i);
+        		} catch (PackageManager.NameNotFoundException e) {
+        				
+        		}     		
+        	}
+        
+      });
 	}
+	
 }
